9947b3bae4491ddd7252f6ac038475f63f5a8211
Guangxin Mi <62838005+miguagnxin@users.noreply.github.com>
2025-03-24 16:39:08 +1100

    Add files via upload

--- src/fineweb.py
+++ src/fineweb.py
@@ -15,8 +15,8 @@ from datasets import load_dataset # pip install datasets
 from tqdm import tqdm # pip install tqdm
 
 # ------------------------------------------
-local_dir = "edu_fineweb10B"
-remote_name = "sample-10BT"
+local_dir = "Tweets_Australian_Cities_Tokens"
+remote_name = "SouthernCrossAI/Tweets_Australian_Cities"
 shard_size = int(1e8) # 100M tokens per shard, total of 100 shards
 
 # create the cache the local directory if it doesn't exist yet
@@ -24,7 +24,7 @@ DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
 os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 
 # download the dataset
-fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
+fw = load_dataset(remote_name, split="train")
 
 # init the tokenizer
 enc = tiktoken.get_encoding("gpt2")
@@ -32,7 +32,7 @@ eot = enc._special_tokens['<|endoftext|>'] # end of text token
 def tokenize(doc):
     # tokenizes a single document and returns a numpy array of uint16 tokens
     tokens = [eot] # the special <|endoftext|> token delimits all documents
-    tokens.extend(enc.encode_ordinary(doc["text"]))
+    tokens.extend(enc.encode_ordinary(doc["tweet"]))
     tokens_np = np.array(tokens)
     assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
     tokens_np_uint16 = tokens_np.astype(np.uint16)
