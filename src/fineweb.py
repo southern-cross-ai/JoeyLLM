ec4ed02a56bfac3509067f748588b68641b6a637
Guangxin Mi <62838005+miguagnxin@users.noreply.github.com>
2025-03-25 13:24:09 +1100

    Update fineweb.py

--- src/fineweb.py
+++ src/fineweb.py
@@ -1,10 +1,7 @@
 """
-FineWeb-Edu dataset (for srs pretraining)
-https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu
 Downloads and tokenizes the data and saves data shards to disk.
 Run simply as:
 $ python fineweb.py
-Will save shards to the local directory "edu_fineweb10B".
 """
 
 import os
