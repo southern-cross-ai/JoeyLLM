b63ee05802d4a1bec89a51d8d5905662aaf7eacc
Matthew Altenburg <Matthew Altenburg>
2025-04-26 21:12:23 +1000

    cleaning files

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from omegaconf import DictConfig, OmegaConf
 import os
 import sys
 
-@hydra.main(config_path="../configs", config_name="config", version_base=None)
+@hydra.main(config_path="configs", config_name="config", version_base=None)
 
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
