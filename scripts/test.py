f2d108e20b45517f82106bed8a3f5fc2881cb345
Matthew Altenburg <Matthew Altenburg>
2025-04-27 10:12:12 +1000

    moved test.py to script dir

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,14 @@
+import sys
+import os
+
+# Add project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from model import JoeyLLM, Test_Model 
 
-@hydra.main(config_path="configs", config_name="config", version_base=None)
+@hydra.main(config_path="../configs", config_name="config", version_base=None)
 
 def main(cfg: DictConfig):
     print("----------Current config!----------")
