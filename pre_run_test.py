6f718fac98c27530c5a15bededc5d4bbb1033086
Matthew Altenburg <Matthew Altenburg>
2025-04-30 13:59:08 +1000

    fixed test map

--- pre_run_test.py
+++ pre_run_test.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add project root to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from model import JoeyLLM, Test_Model 
