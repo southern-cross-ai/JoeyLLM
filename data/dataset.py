bc4fd317f8ce130a4f12c625d3d1e2fe48a57452
Matthew Altenburg <Matthew Altenburg>
2025-05-14 14:34:43 +1000

    refactor(data): refactored Dataloaders function for better readability

--- data/dataset.py
+++ data/dataset.py
@@ -1,22 +1,26 @@
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
-def Dataloaders(configs):
+def Dataloaders(dataset_in: str, batch_size: int, columns: list, shuffle: bool):
+    """
+    Loads dataset and returns PyTorch dataloaders.
+    """
     print("---------Starting Data Step!----------")
-    dataset_in = configs.dataset_in
-    batch_size = configs.batch_size
-    columns = configs.columns
-    shuffle = configs.shuffle
+    print(f"-----Loading Dataset: {dataset_in} -----")
 
-    print(f"-----Loading Dataset {dataset_in} From HuggingFace!-----")
     dataset = load_dataset(dataset_in, split=None)
     dataset = dataset.with_format("torch", columns=columns)
 
-    print("----------Creating Dataloaders!----------")
-    train_loader = DataLoader(dataset['train'], batch_size=batch_size, shuffle=shuffle)
-    val_loader = DataLoader(dataset['validation'], batch_size=batch_size, shuffle=False) if 'validation' in dataset else None
-    test_loader = DataLoader(dataset['test'], batch_size=batch_size, shuffle=False) if 'test' in dataset else None
-    print("----------Dataloaders Done!----------")
+    print("----------Creating Dataloaders----------")
 
+    train_loader = DataLoader(dataset['train'], batch_size=batch_size, shuffle=shuffle)
+
+    val_loader = DataLoader(dataset['validation'], batch_size=batch_size, shuffle=False) \
+        if 'validation' in dataset else None
+
+    test_loader = DataLoader(dataset['test'], batch_size=batch_size, shuffle=False) \
+        if 'test' in dataset else None
+
+    print("----------Dataloaders Ready----------")
     return train_loader, val_loader, test_loader
 
