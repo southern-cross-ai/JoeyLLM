de2a08f11107ff123d0d1e4e99fd9becd83ac117
Guangxin Mi <62838005+miguagnxin@users.noreply.github.com>
2025-08-06 09:33:20 +1000

    Update fineweb.py

--- src/fineweb.py
+++ src/fineweb.py
@@ -84,3 +84,4 @@ if __name__ == "__main__":
     parser.add_argument("--field", type=str, default="tweet", help="Field to tokenize (default: 'tweet')")
     args = parser.parse_args()
     main(args)
+
